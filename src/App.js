da1ceb699669fb6e826b9556bd64ec019679a70c
aakashpatidar <aakash.patidar@engineerbabu.in>
2020-02-21 10:43:09 +0530

    enzyme plugin added for testing

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import {connect} from 'react-redux';
 import * as actions from './store/actions/index';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+const asyncCheckout = asyncComponent(() =>{
+  return import('./containers/Checkout/Checkout');
+})
+
+const asyncOrder = asyncComponent(() =>{
+  return import('./containers/Orders/Orders');
+})
+
+const asyncAuth = asyncComponent(() =>{
+  return import ('./containers/Auth/Auth');
+})
 class App extends Component {
   componentDidMount(){
     this.props.onTryAutoSignup()
@@ -16,7 +26,7 @@ class App extends Component {
   render () {
     let route = (
       <Switch>
-      <Route path="/auth" component={Auth} />
+      <Route path="/auth" component={asyncAuth} />
       <Route path="/" exact component={BurgerBuilder} />
       <Redirect to="/" />
       </Switch>
@@ -24,10 +34,10 @@ class App extends Component {
       if(this.props.isAuthenticated){
       route = (
           <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
+            <Route path="/checkout" component={asyncCheckout} />
+            <Route path="/orders" component={asyncOrder} />
             <Route path="/logout" component={Logout} />
-            <Route path="/auth" component={Auth} /> 
+            <Route path="/auth" component={asyncAuth} /> 
             <Route path="/" exact component={BurgerBuilder} />
             <Redirect to="/" />
           </Switch>
